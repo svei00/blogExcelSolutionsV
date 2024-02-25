3430d9d53d40d0626b7a2742e9a4e29a104e0cf7
svei00 <79055634+svei00@users.noreply.github.com>
2024-02-24 23:11:49 -0800

    Created Header/Navbar Component

---
+++ client/src/components/Header.jsx
@@ -0,0 +1,59 @@
+import { Button, Navbar, TextInput } from "flowbite-react";
+import { Link, useLocation } from "react-router-dom";
+import logo from "../assets/LogoExcelv2_Trim_803x230.png";
+import { AiOutlineSearch } from "react-icons/ai";
+import { FaMoon } from "react-icons/fa";
+
+export default function Header() {
+  const path = useLocation().pathname;
+  return (
+    <Navbar className="border-b-2">
+      <Link
+        to="/"
+        className="self-center whitespace-nowrap text-sm sm:text-xl font-semibold dark:text-white"
+      >
+        <span className="flex items-end justify-center ">
+          <img src={logo} alt="Logo Excel Solutions" className="h-10" />
+          <span className="text-greenEx"> Blog</span>
+        </span>
+        {/* <span className="px-2 py-1 bg-gradient-to-r from-blueEx to-greenEx rounded-lg text-white">
+          Excel Solutions®
+        </span>
+        Blog */}
+      </Link>
+      <form>
+        <TextInput
+          type="text"
+          placeholder="Search..."
+          rightIcon={AiOutlineSearch}
+          className="hidden lg:inline"
+        />
+      </form>
+      <Button className="w-12 h-10 lg:hidden" color="gray" pill>
+        <AiOutlineSearch />
+      </Button>
+      <div className="flex gap-2 md:order-2">
+        <Button className="w-12 h-10 hidden sm:inline" color="gray" pill>
+          <FaMoon />
+        </Button>
+        <Link to={"/sign-in"}>
+          <Button className="bg-gradient-to-r from-blueEx to-greenEx">
+            Sign In
+          </Button>
+        </Link>
+        <Navbar.Toggle />
+      </div>
+      <Navbar.Collapse>
+        <Navbar.Link active={path === "/"} as={"div"}>
+          <Link to="/">Home</Link>
+        </Navbar.Link>
+        <Navbar.Link active={path === "/about"} as={"div"}>
+          <Link to="/about">About</Link>
+        </Navbar.Link>
+        <Navbar.Link active={path === "/projects"} as={"div"}>
+          <Link to="/projects">Projects</Link>
+        </Navbar.Link>
+      </Navbar.Collapse>
+    </Navbar>
+  );
+}
--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -1,8 +1,18 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+    "node_modules/flowbite-react/lib/esm/**/*.js",
+  ],
   theme: {
+    colors: {
+      transparent: "transparent",
+      current: "currentColor",
+      blueEx: "#3182DF",
+      greenEx: "#21B868",
+    },
     extend: {},
   },
-  plugins: [],
+  plugins: [require("flowbite/plugin")],
 };
