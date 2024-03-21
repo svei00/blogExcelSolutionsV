1513e6c16f591ecce9f868344e864a9fac16c2e9
svei00 <79055634+svei00@users.noreply.github.com>
2024-03-20 19:42:32 -0700

    Fixing the Profile Picture border

--- client/src/components/DahsProfile.jsx
+++ client/src/components/DahsProfile.jsx
@@ -7,11 +7,12 @@ export default function DahsProfile() {
     <div className="max-w-lg mx-auto p-3 w-full">
       <h1 className="my-7 text-center font-semibold text-3xl">Profile</h1>
       <form className="flex flex-col gap-4">
-        <div className="w-32 h-32 self-center cursor-pointer shadow-md overflow-hidden rounded-full">
+        <div className="relative w-32 h-32 self-center cursor-pointer shadow-md overflow-hidden rounded-full">
           <img
             src={currentUser.profilePicture}
             alt="User Image"
-            className="rounded-full w-full h-full object-cover border-8 border-[lightgray]"
+            className="rounded-full w-full h-full object-cover"
+            style={{ border: "8px solid lightgray" }}
           />
         </div>
         <TextInput
