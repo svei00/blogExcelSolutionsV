bd19bf56aa9162154a6962270d17f52e44aef059
svei00 <79055634+svei00@users.noreply.github.com>
2024-05-18 22:59:24 -0700

    Still working on Add call to action to the post page

--- client/src/components/CallToAction.jsx
+++ client/src/components/CallToAction.jsx
@@ -1,9 +1,14 @@
+import { Button } from "flowbite-react";
+
 export default function CallToAction() {
   return (
     <div>
       <div className="">
         <h2>Want to learn more about Excel?</h2>
         <p>Checkout these resources with 100% practical exercises</p>
+        <Button className="bg-gradient-to-r from-blueEx to-greenEx">
+          Learn More
+        </Button>
       </div>
       <div className="p-7">
         <img
