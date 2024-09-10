3395d3d3db241237ec837ac46345aa74dd82e7dd
svei00 <79055634+svei00@users.noreply.github.com>
2024-09-09 20:48:06 -0700

    Working on React Helmet

--- client/src/pages/PostPage.jsx
+++ client/src/pages/PostPage.jsx
@@ -60,6 +60,12 @@ export default function PostPage() {
         <Spinner size="xl" />
       </div>
     );
+
+  const getMetaDescription = (content) => {
+    // Strip HTML tagas and get first 160 characters
+    return content.replace(/<[^>]*>?/gm, "").substring(0, 160);
+  };
+
   return (
     <main className="p-3 flex flex-col max-w-6xl mx-auto min-h-screen">
       <h1 className="text-3xl mt-10 p-3 text-center font-serif max-w-2xl mx-auto lg:text-4xl">
