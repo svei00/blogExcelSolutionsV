b393e2c7aae9e413328a37cbf875d07d0c984af0
svei00 <79055634+svei00@users.noreply.github.com>
2024-09-13 23:28:50 -0700

    Rendering the helpmet app

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -14,35 +14,37 @@ import PostPage from "./pages/PostPage";
 import ScrollToTop from "./components/ScrollToTop";
 import Search from "./pages/Search";
 import HeaderLayout from "./components/HeaderLayout";
-import { Helmet } from "react-helmet";
+import { HelmetProvider, Helmet } from "react-helmet-async";
 
 export default function App() {
   return (
-    <BrowserRouter>
-      <Helmet>
-        <title>ExcelSolutionsV Blog</title>
-        <meta name="description" content="Welcome to ExcelSolutionsV Blot!" />
-      </Helmet>
-      <ScrollToTop />
-      <HeaderLayout>
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/about" element={<About />} />
-          <Route path="/sign-in" element={<SignIn />} />
-          <Route path="/sign-up" element={<SignUp />} />
-          <Route path="/search" element={<Search />} />
-          <Route element={<PrivateRoute />}>
-            <Route path="/dashboard" element={<Dashboard />} />
-          </Route>
-          <Route element={<OnlyAdminPrivateRoute />}>
-            <Route path="/create-post" element={<CreatePost />} />
-            <Route path="/update-post/:postId" element={<UpdatePost />} />
-          </Route>
-          <Route path="/projects" element={<Projects />} />
-          <Route path="/post/:postSlug" element={<PostPage />} />
-        </Routes>
-      </HeaderLayout>
-      <Footer />
-    </BrowserRouter>
+    <HelmetProvider>
+      <BrowserRouter>
+        <Helmet>
+          <title>ExcelSolutionsV Blog</title>
+          <meta name="description" content="Welcome to ExcelSolutionsV Blot!" />
+        </Helmet>
+        <ScrollToTop />
+        <HeaderLayout>
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/about" element={<About />} />
+            <Route path="/sign-in" element={<SignIn />} />
+            <Route path="/sign-up" element={<SignUp />} />
+            <Route path="/search" element={<Search />} />
+            <Route element={<PrivateRoute />}>
+              <Route path="/dashboard" element={<Dashboard />} />
+            </Route>
+            <Route element={<OnlyAdminPrivateRoute />}>
+              <Route path="/create-post" element={<CreatePost />} />
+              <Route path="/update-post/:postId" element={<UpdatePost />} />
+            </Route>
+            <Route path="/projects" element={<Projects />} />
+            <Route path="/post/:postSlug" element={<PostPage />} />
+          </Routes>
+        </HeaderLayout>
+        <Footer />
+      </BrowserRouter>
+    </HelmetProvider>
   );
 }
