54a12e54260529f67b84b7464fe425c3e4f8a001
svei00 <79055634+svei00@users.noreply.github.com>
2024-09-13 23:42:44 -0700

    Fixing helmet

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,18 +1,21 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import App from "./App.jsx";
-import "./index.css";
-import { store, persistor } from "./redux/store.js";
+import { HelmetProvider } from "react-helmet-async";
 import { Provider } from "react-redux";
 import { PersistGate } from "redux-persist/integration/react";
+import { store, persistor } from "./redux/store.js";
 import ThemeProvider from "./components/ThemeProvider.jsx";
+import App from "./App.jsx";
+import "./index.css";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
-  <PersistGate persistor={persistor}>
+  <HelmetProvider>
     <Provider store={store}>
-      <ThemeProvider>
-        <App />
-      </ThemeProvider>
+      <PersistGate loading={null} persistor={persistor}>
+        <ThemeProvider>
+          <App />
+        </ThemeProvider>
+      </PersistGate>
     </Provider>
-  </PersistGate>
+  </HelmetProvider>
 );
