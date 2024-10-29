2427e9abd464aae8f9eabf08de763d418456bfd5
svei00 <79055634+svei00@users.noreply.github.com>
2024-10-28 18:24:09 -0700

    added header 3 and 4 for better SEO performance

--- client/src/components/CustomReactQuill.jsx
+++ client/src/components/CustomReactQuill.jsx
@@ -56,7 +56,7 @@ const CustomReactQuill = ({ value, onChange }) => {
   const modules = {
     toolbar: {
       container: [
-        [{ header: [1, 2, false] }],
+        [{ header: [1, 2, 3, 4, false] }],
         ["bold", "italic", "underline", "strike"],
         ["blockquote", "code-block"], // Includes the code-block tool
         [{ list: "ordered" }, { list: "bullet" }],
