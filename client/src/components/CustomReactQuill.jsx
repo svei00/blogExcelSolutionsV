83c8dabe3a75e279b5406cb2b23a4f95dcbc43fd
svei00 <79055634+svei00@users.noreply.github.com>
2024-11-06 23:29:08 -0900

    Chamnged the color of text in react quill component

--- client/src/components/CustomReactQuill.jsx
+++ client/src/components/CustomReactQuill.jsx
@@ -130,7 +130,7 @@ const CustomReactQuill = ({ value, onChange }) => {
         ref={quillRef}
         theme="snow"
         placeholder="Create a story..."
-        className="h-72 mb-12 border-greenEx dark:text-white"
+        className="h-72 mb-12 border-greenEx dark:text-red-500"
         required
         value={value}
         onChange={onChange}
