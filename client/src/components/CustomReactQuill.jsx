8be617d62843f39157493e86b56f7d1e04f2f1fd
svei00 <79055634+svei00@users.noreply.github.com>
2024-11-09 15:13:28 -0800

    change the placeholder from the component CustomReactquit to the page CreatePost to render propoerly v5

--- client/src/components/CustomReactQuill.jsx
+++ client/src/components/CustomReactQuill.jsx
@@ -130,7 +130,7 @@ const CustomReactQuill = ({ value, onChange, placeholder }) => {
         ref={quillRef}
         theme="snow"
         placeholder={placeholder}
-        className="h-72 mb-12 dark:placeholder-white"
+        className="h-72 mb-12"
         required
         value={value}
         onChange={onChange}
--- client/src/pages/CreatePost.jsx
+++ client/src/pages/CreatePost.jsx
@@ -184,6 +184,7 @@ export default function CreatePost() {
         /> */}
 
         <CustomReactQuill
+          dark:placeholder-white
           value={formData.content || ""}
           onChange={(value) => {
             setFormData({ ...formData, content: value });
